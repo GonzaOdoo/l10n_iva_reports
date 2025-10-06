cb613879fc2b4693f0f0244bd8aaab5405ba954c
GonzaOdoo <zelayagonzalo33@gmail.com>
2025-10-05 23:09:42 -0300

    Actualizada validacion de vat

--- models/res_partner.py
+++ models/res_partner.py
@@ -11,7 +11,7 @@ class ResPartner(models.Model):
     )
 
     def check_vat_py(self, vat):
-        return vat.isdigit() and len(vat) == 8
+        return vat.isdigit() and 3 <= len(vat) <= 8
 
     #l10n_xma_indentification_type = fields.Selection(
         #selection = [('11','RUC'),('12','Cédula de identidad'),('13','Pasaporte'),('14','Cédula extranjero'),('15','Sin nombre'),('16','Diplomático'),('17','Identificación Tributaria')], string='Tipo de identificación',default='11')
